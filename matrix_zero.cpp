1f12018c72ff91414833052f67f6ceb16f9c4021
Hansy <hansy2808@gmail.com>
2026-05-22 14:04:20 +0530

    Implement setZeroes function for matrix

---
+++ matrix_zero.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        //col[n]=matrix[..][0]
+        //row[n]=matrix[0][..]
+        int n = matrix.size();
+int m = matrix[0].size();
+        int col0=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    //mark the i-th row
+                    matrix[i][0]=0;
+                    //mark the j-th col
+                    if(j!=0){
+                        matrix[0][j]=0;
+                    }
+                    else{
+                        col0=0;
+                    }
+                }
+                }    }
+        
+for(int i=1;i<n;i++){
+    for(int j=1;j<m;j++){
+        if(matrix[i][j]!=0){
+            //check for row[n],col[n]
+                if(matrix[i][0]==0||matrix[0][j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+    }
+    
+if(matrix[0][0]==0){
+    for(int j=0;j<m;j++){
+        matrix[0][j]=0;
+    }
+}
+if(col0==0){
+    for(int i=0;i<n;i++){
+        matrix[i][0]=0;
+    }
+}
+
+
+    }
+};
